d6f5eecd6bac95809f62e3dea90586ee467ae131
Mac1 <mac1@MacBook-Pro-Mac1.local>
2020-02-15 10:42:34 +0200

    add task

---
+++ src/practice_2/task_1.js
@@ -0,0 +1,31 @@
+const tree = {
+	value: 1,
+	next: [
+		{
+			value: 6,
+			next: null
+		},
+		{
+			value: 2,
+			next: [
+				{
+					value: 77,
+					next: null
+				},
+				{
+					value: 4,
+					next: [
+                        {
+                            value: 55,
+                            next: null
+                        },
+                        {
+                            value: -5,
+                            next: null
+                        }
+                    ]
+				}
+			]
+		}
+	]
+};
